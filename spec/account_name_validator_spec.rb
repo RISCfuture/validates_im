baa72e3b68f1afc5063f0322eba3bd1ee8dcb18b
Tim Morgan <git@timothymorgan.info>
2022-09-18 12:37:43 -0700

    Spec updates

--- spec/account_name_validator_spec.rb
+++ spec/account_name_validator_spec.rb
@@ -18,12 +18,12 @@ describe AccountNameValidator do
 
   describe ".error_key_prefix" do
     it "should be the downcased name of the validator by default" do
-      SpecSupport::TestAccountNameValidator.error_key_prefix.should eql(:test_account_name)
+      expect(SpecSupport::TestAccountNameValidator.error_key_prefix).to eql(:test_account_name)
     end
 
     it "should be able to be set" do
       SpecSupport::TestAccountNameValidator.error_key_prefix(:foo)
-      SpecSupport::TestAccountNameValidator.error_key_prefix.should eql(:foo)
+      expect(SpecSupport::TestAccountNameValidator.error_key_prefix).to eql(:foo)
       SpecSupport::TestAccountNameValidator.instance_variable_set :@error_key_prefix, nil
     end
   end
@@ -31,31 +31,31 @@ describe AccountNameValidator do
   describe "#validate_each" do
     it "should do nothing if given nil and :allow_nil is set" do
       SpecSupport::TestAccountNameValidator.new(attributes: :field, allow_nil: true).validate_each(@model, :field, nil)
-      @model.errors.should be_empty
+      expect(@model.errors).to be_empty
     end
 
     it "should do nothing if given a blank value and :allow_blank is set" do
       SpecSupport::TestAccountNameValidator.new(attributes: :field, allow_blank: true).validate_each(@model, :field, "")
-      @model.errors.should be_empty
+      expect(@model.errors).to be_empty
     end
 
     it "should add an error to the record if any validation fails" do
       SpecSupport::TestAccountNameValidator.add_validation(:not_foo) { |value| value == 'foo' }
       SpecSupport::TestAccountNameValidator.new(attributes: :field).validate_each(@model, :field, 'bar')
-      @model.errors[:field].should_not be_empty
-      @model.errors[:field].first.should include('test_account_name_not_foo')
+      expect(@model.errors[:field]).not_to be_empty
+      expect(@model.errors[:field].first).to include('test_account_name_not_foo')
     end
 
     it "should use a :message option for the message" do
       SpecSupport::TestAccountNameValidator.add_validation(:not_foo) { |value| value == 'foo' }
       SpecSupport::TestAccountNameValidator.new(attributes: :field, message: 'bar').validate_each(@model, :field, 'bar')
-      @model.errors[:field].should eql([ 'bar' ])
+      expect(@model.errors[:field]).to eql([ 'bar' ])
     end
     
     it "should typecast the value to a string" do
       SpecSupport::TestAccountNameValidator.add_validation(:not_foo) { |value| value == 'foo' }
       SpecSupport::TestAccountNameValidator.new(attributes: :field).validate_each(@model, :field, :foo)
-      @model.errors.should be_empty
+      expect(@model.errors).to be_empty
     end
   end
 
@@ -67,13 +67,13 @@ describe AccountNameValidator do
     it "should add a validation block" do
       SpecSupport::TestAccountNameValidator.add_validation(:not_foo) { |value| value == 'foo' }
       @validator.validate_each(@model, :field, 'bar')
-      @model.errors[:field].should_not be_empty
+      expect(@model.errors[:field]).not_to be_empty
     end
 
     it "should do nothing if no block is given" do
       SpecSupport::TestAccountNameValidator.add_validation(:not_foo)
       @validator.validate_each(@model, :field, 'bar')
-      @model.errors[:field].should be_empty
+      expect(@model.errors[:field]).to be_empty
     end
   end
 
@@ -81,8 +81,8 @@ describe AccountNameValidator do
     it "should ensure a minimum length" do
       SpecSupport::TestAccountNameValidator.min_length 5
       SpecSupport::TestAccountNameValidator.new(attributes: :field).validate_each(@model, :field, '1234')
-      @model.errors[:field].should_not be_empty
-      @model.errors[:field].first.should include('too_short')
+      expect(@model.errors[:field]).not_to be_empty
+      expect(@model.errors[:field].first).to include('too_short')
     end
   end
 
@@ -90,8 +90,8 @@ describe AccountNameValidator do
     it "should ensure a maximum length" do
       SpecSupport::TestAccountNameValidator.max_length 5
       SpecSupport::TestAccountNameValidator.new(attributes: :field).validate_each(@model, :field, '123456')
-      @model.errors[:field].should_not be_empty
-      @model.errors[:field].first.should include('too_long')
+      expect(@model.errors[:field]).not_to be_empty
+      expect(@model.errors[:field].first).to include('too_long')
     end
   end
 
@@ -99,8 +99,8 @@ describe AccountNameValidator do
     it "should check for invalid chars" do
       SpecSupport::TestAccountNameValidator.valid_chars '[A-Z]'
       SpecSupport::TestAccountNameValidator.new(attributes: :field).validate_each(@model, :field, 'A!Z')
-      @model.errors[:field].should_not be_empty
-      @model.errors[:field].first.should include('invalid_chars')
+      expect(@model.errors[:field]).not_to be_empty
+      expect(@model.errors[:field].first).to include('invalid_chars')
     end
   end
 
@@ -108,8 +108,8 @@ describe AccountNameValidator do
     it "should check for an invalid first character" do
       SpecSupport::TestAccountNameValidator.first_char '[A-Z]'
       SpecSupport::TestAccountNameValidator.new(attributes: :field).validate_each(@model, :field, 'abc')
-      @model.errors[:field].should_not be_empty
-      @model.errors[:field].first.should include('invalid_first_char')
+      expect(@model.errors[:field]).not_to be_empty
+      expect(@model.errors[:field].first).to include('invalid_first_char')
     end
   end
 end
